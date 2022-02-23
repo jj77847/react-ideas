4a634e455a6f47cc4a195d2c9140aefe1d7feb7e
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-02-23 12:14:42 +0000

    img.css test

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
+import "../styles/Img.css";
 
 // We declare an object called styles that will contain a few objects for card and heading styles
 // Notice that each key lists CSS styles in camel case
 const styles = {
   card: {
     margin: 20,
-    background: '#e8eaf6',
+    background: "#e8eaf6",
   },
   heading: {
-    background: '#3f51b5',
+    background: "#3f51b5",
     minHeight: 50,
     lineHeight: 3.5,
-    fontSize: '1.2rem',
-    color: 'white',
-    padding: '0 20px',
+    fontSize: "1.2rem",
+    color: "white",
+    padding: "0 20px",
   },
   content: {
     padding: 20,

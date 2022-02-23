0764ddb04df84ea863200eec6b23deeb7d468427
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-02-23 11:45:49 +0000

    files and folders

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import Footer from ".components/Footer";
 
-import Home from './pages/Home';
-import Profile from './pages/Profile';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
-
+// In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function App() {
   return (
-    <ApolloProvider client={client}>
-      {/* Wrap page elements in Router component to keep track of location state */}
-      <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
-            {/* Define routes to render different page components at different paths */}
-            <Route exact path="/">
-              <Home />
-            </Route>
-            {/* Define a route that will take in variable data */}
-            <Route exact path="/profiles/:profileId">
-              <Profile />
-            </Route>
-          </div>
-          <Footer />
-        </div>
-      </Router>
-    </ApolloProvider>
+    <div>
+      <Header />
+      <Navbar />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Footer />
+    </div>
   );
 }
 

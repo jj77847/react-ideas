4cea75b4bdbe2821c9e631ae220b604b9ed21ec8
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-02-23 11:54:18 +0000

    footer.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Card from "./components/Card";
-import Footer from ".components/Footer";
+import Footer from "./components/Footer";
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function App() {

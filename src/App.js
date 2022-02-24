9d2c85110d688cd28d15d8806cb87103e25fa540
jj77847 <81172135+jj77847@users.noreply.github.com>
2022-02-24 16:11:14 +0000

    testing app.js

--- src/App.js
+++ src/App.js
@@ -2,36 +2,17 @@ import React from "react";
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
-import AboutMe from "./components/AboutMe";
-import Experience from "./components/Experience";
-import Header from "./components/Header";
-import Quals from "./components/Quals";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer";
-import Education from "./components/Education";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
+// import AboutMe from "./components/AboutMe";
+// import Experience from "./components/Experience";
+// import Header from "./components/Header";
+// import Quals from "./components/Quals";
+// import Projects from "./components/Projects";
+// import Footer from "./components/Footer";
+// import Education from "./components/Education";
+// import Contact from "./components/Contact";
+// import Navbar from "./components/Navbar";
 
-// import navbar from "./components/navbar"
-
-ReactDOM.render(
-    <Router>
-       <Switch>
-					<Header/> //new
-		      <Route exact path="/" component={Homepage}/>
-          <Route exact path="/about-me" component={AboutMe }/>
-          <Route  path='/contact' component={Contact} />
-          <Route  path='/education' component={Education} />
-          <Route  path='/experience' component={Experience} />
-          <Route  path='/footer' component={Footer} />
-          <Route  path='/header' component={Header} />
-          <Route  path='/projects' component={Projects} />
-          <Route  path='/quals' component={Quals} />
-	    </Switch>
-    </Router>,
-    document.getElementById('root')};
-
-function App() { 
+function App() {
   return (
     <div>
       <Header />
@@ -49,9 +30,23 @@ function App() {
 
 export default App;
 
+// import navbar from "./components/navbar"
+
+// ReactDOM.render(
+//     <Router>
+//        <Switch>
+// 					<Header/> //new
+// 		      <Route exact path="/" component={Homepage}/>
+//           <Route exact path="/about-me" component={AboutMe }/>
+//           <Route  path='/contact' component={Contact} />
+//           <Route  path='/education' component={Education} />
+//           <Route  path='/experience' component={Experience} />
+//           <Route  path='/footer' component={Footer} />
+//           <Route  path='/header' component={Header} />
+//           <Route  path='/projects' component={Projects} />
+//           <Route  path='/quals' component={Quals} />
+// 	    </Switch>
+//     </Router>,
+//     document.getElementById('root')};
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -4,87 +4,87 @@ import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-const Header = () => {
-  return (
-    <div>
-      <p>Header</p>
-    </div>
-  );
-};
-
-// const Navbar = () => {
+// const Header = () => {
 //   return (
 //     <div>
-//       <h1>Navbar</h1>
-//       <Link to="navbar">Go to Navbar</Link>
+//       <p>Header</p>
 //     </div>
 //   );
 // };
 
-const AboutMe = () => {
-  return (
-    <div>
-      <h1>About Me</h1>
-      <Link to="about-me">Go to AboutMe</Link>
-    </div>
-  );
-};
+// // const Navbar = () => {
+// //   return (
+// //     <div>
+// //       <h1>Navbar</h1>
+// //       <Link to="navbar">Go to Navbar</Link>
+// //     </div>
+// //   );
+// // };
 
-const Education = () => {
-  const Quals = () => {
-    return (
-      <div>
-        <h1>Qualifications</h1>
-        <Link to="quals">Go to Quals</Link>
-      </div>
-    );
-  };
-  const Experience = () => {
-    return (
-      <div>
-        <h1>Experience</h1>
-        <Link to="experience">Go to Experience</Link>
-      </div>
-    );
-  };
+// const AboutMe = () => {
+//   return (
+//     <div>
+//       <h1>About Me</h1>
+//       <Link to="about-me">Go to AboutMe</Link>
+//     </div>
+//   );
+// };
 
-  const Education = () => {
-  return (
-    <div>
-      <h1>Education</h1>
-      <Link to="education">Go to Education</Link>
-    </div>
-  );
-};
+// const Education = () => {
+//   const Quals = () => {
+//     return (
+//       <div>
+//         <h1>Qualifications</h1>
+//         <Link to="quals">Go to Quals</Link>
+//       </div>
+//     );
+//   };
+//   const Experience = () => {
+//     return (
+//       <div>
+//         <h1>Experience</h1>
+//         <Link to="experience">Go to Experience</Link>
+//       </div>
+//     );
+//   };
 
-const Projects = () => {
-  return (
-    <div>
-      <h1>Projects</h1>
-      <Link to="projects">Go to Projects</Link>
-    </div>
-  );
-};
+//   const Education = () => {
+//   return (
+//     <div>
+//       <h1>Education</h1>
+//       <Link to="education">Go to Education</Link>
+//     </div>
+//   );
+// };
 
-const Footer = () => {
-  return (
-    <div>
-      <h1>Footer</h1>
-      <Link to="footer">Go to Footer</Link>
-    </div>
-  );
-};
+// const Projects = () => {
+//   return (
+//     <div>
+//       <h1>Projects</h1>
+//       <Link to="projects">Go to Projects</Link>
+//     </div>
+//   );
+// };
 
-export {
-  Homepage,
-  Header,
-//   Navbar,
-  AboutMe,
-  Quals,
-  Experience,
-  Projects,
-  Education,
-  Footer,
-}; //new
+// const Footer = () => {
+//   return (
+//     <div>
+//       <h1>Footer</h1>
+//       <Link to="footer">Go to Footer</Link>
+//     </div>
+//   );
+// };
+
+// export {
+//   Homepage,
+//   Header,
+// //   Navbar,
+//   AboutMe,
+//   Quals,
+//   Experience,
+//   Projects,
+//   Education,
+//   Footer,
+// }; //new
